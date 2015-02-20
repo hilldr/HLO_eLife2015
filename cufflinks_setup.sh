77fd967923d64ce7ba34f8c0d35374858a20821f
hilldr <hilldr@med.umich.edu>
2015-02-20 10:05:53 -0500

    Commit message

--- cufflinks_setup.sh
+++ cufflinks_setup.sh
@@ -1,4 +1,27 @@
 #! /bin/bash
+cufflinks -p 4 -o ME1 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_Klein551ME1_accepted_hits.bam 
+cufflinks -p 4 -o ME2 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_Klein551ME2_accepted_hits.bam 
+cufflinks -p 4 -o ME3 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_Klein551ME3_accepted_hits.bam 
+cufflinks -p 4 -o MD1 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_Klein551MD1_accepted_hits.bam 
+cufflinks -p 4 -o MD2 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_Klein551MD2_accepted_hits.bam 
+cufflinks -p 4 -o MD3 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_Klein551MD3_accepted_hits.bam 
+cufflinks -p 4 -o ERR315341 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_ERR315341_sorted.bam 
+cufflinks -p 4 -o ERR315346 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_ERR315346_sorted.bam 
+cufflinks -p 4 -o ERR315353 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_ERR315353_sorted.bam 
+cufflinks -p 4 -o ERR315424 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_ERR315424_sorted.bam 
+cufflinks -p 4 -o ERR315439 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_ERR315439_sorted.bam 
+cufflinks -p 4 -o 41681--multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_41681_sorted.bam 
+cufflinks -p 4 -o 41676--multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_41676_sorted.bam 
+cufflinks -p 4 -o 41677--multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_41677_sorted.bam 
+cufflinks -p 4 -o 41678--multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_41678_sorted.bam 
+cufflinks -p 4 -o 41679--multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_41679_sorted.bam 
+cufflinks -p 4 -o 41680--multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_41680_sorted.bam 
+cufflinks -p 4 -o SRR786753 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_SRR786753_sorted.bam 
+cufflinks -p 4 -o SRR786754 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_SRR786754_sorted.bam 
+cufflinks -p 4 -o SRR786756 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_SRR786756_sorted.bam 
+cufflinks -p 4 -o SRR786762 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_SRR786762_sorted.bam 
+cufflinks -p 4 -o SRR786769 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_SRR786769_sorted.bam 
+cufflinks -p 4 -o SRR786780 --multi-read-correct --compatible-hits-norm --upper-quartile-norm --GTF hg19_genes.gtf Sample_SRR786780_sorted.bam 
 cuffmerge -g hg19_genes.gtf -s hg19.fa -p 4 assembly.txt
 cuffquant -p 4 -o ME1 --max-mle-iterations 100000 -v --multi-read-correct merged_asm/merged.gtf Sample_Klein551ME1_accepted_hits.bam
 cuffquant -p 4 -o ME2 --max-mle-iterations 100000 -v --multi-read-correct merged_asm/merged.gtf Sample_Klein551ME2_accepted_hits.bam
